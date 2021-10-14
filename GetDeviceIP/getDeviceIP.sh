bed8fb248e2e18ff09a406c9f1b329cde7fed8cb
TCop <tommo9898@gmail.com>
2021-10-14 19:55:21 +0800

    Update getDeviceIP.sh

--- GetDeviceIP/getDeviceIP.sh
+++ GetDeviceIP/getDeviceIP.sh
@@ -6,7 +6,7 @@ line=$(arp -a | grep $mac)
 ip=$(echo $line | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
 
 if [[ $ip = "" ]]; then
-	echo "The device could not be found. Ensure it is powered and connected to the network."
+	echo "The device could not be found. See user manual for troubleshooting."
 else
 	echo "The device was found with the IP address $ip"
 	echo "Would you like to open the web configuration? [Y/n]"
